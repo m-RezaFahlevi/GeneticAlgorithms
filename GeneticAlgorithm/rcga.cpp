fe99f79e1dc84260e04d3db3c51149152f6a10c3
m-RezaFahlevi <57177960+m-RezaFahlevi@users.noreply.github.com>
2023-04-08 15:00:24 +0700

    selection and update population method are added

--- GeneticAlgorithm/rcga.cpp
+++ GeneticAlgorithm/rcga.cpp
@@ -33,6 +33,8 @@ double runif(double at_least, double at_most) {
 }
 
 const double boundary[] = {-5.12, 5.12};
+int track_best = 0;
+int track_worst = 0;
 
 double spherefun(double x, double y) {
 	return pow(x, 2.0) + pow(y, 2.0);
@@ -77,30 +79,75 @@ class Population {
 	Individual worst_indiv, best_indiv;
 	public:
 		Population();
+		Individual tournament(int t); // t >= 1 is size of tournament
+		void update_population(Individual offspring);
 		void print_population();
+		void print_worst();
+		void print_best();
 };
 
 Population::Population() {
 	double curr_worst = -99999.0;
 	double curr_best = 99999.0;
+	int search_worst = 0;
+	int search_best = 0;
 	for (int indiv = 0; indiv < NPOPULATION; ++indiv) {
-		Individual member_pop;
+		Individual member_pop; // default constructor;
 		if (member_pop.fitness() < curr_best) {
 			curr_best = member_pop.fitness();
 			best_indiv = member_pop;
+			track_best = search_best;
 		} if (member_pop.fitness() > curr_worst) {
 			curr_worst = member_pop.fitness();
 			worst_indiv = member_pop;
+			track_worst = search_worst;
 		}
 		individuals.push_back(member_pop);
+		search_worst++;
+		search_best++;
+	}
+}
+
+// int t >= 2 is total of tournament
+Individual Population::tournament(int t) {
+	int get_num = runif(0, NPOPULATION - 1);
+	Individual curr_suvivor = individuals.at(get_num);
+	for (int nth_tournament = 2; nth_tournament <= t; ++nth_tournament) {
+		get_num = runif(0, NPOPULATION - 1);
+		Individual gladiator = individuals.at(get_num);
+		if (gladiator.fitness() < curr_suvivor.fitness())
+			curr_suvivor = gladiator;
+	}
+	return curr_suvivor;
+}
+
+void Population::update_population(Individual offspring) {
+	individuals.at(track_worst) = offspring;
+	worst_indiv = offspring;
+	// track best and worst individual (linear search), O(n)
+	int find_worst = 0;
+	int find_best = 0;
+	for (int i = 0; i < NPOPULATION; ++i) {
+		Individual scanned_indiv = individuals.at(i);
+		if (scanned_indiv.fitness() < best_indiv.fitness()) {
+			best_indiv = scanned_indiv;
+			track_best = find_best;
+		} if (scanned_indiv.fitness() > worst_indiv.fitness()) {
+			worst_indiv = scanned_indiv;
+			track_worst = find_worst;
+		}
+		find_worst++;
+		find_best++;
 	}
 }
 
 void Population::print_population() {
-	cout << "Individual \t\t finess\n";
+	cout << " Individual \t\t finess\n";
+	int counter = 0;
 	for (Individual creature: individuals) {
-		creature.print_gene();
+		cout << counter << ". "; creature.print_gene();
 		cout << "\t " << creature.fitness() << "\n";
+		counter++;
 	}
 	cout << "Worst individual : "; worst_indiv.print_gene();
 	cout << "\nFitness value : " << worst_indiv.fitness() << endl;
@@ -108,9 +155,27 @@ void Population::print_population() {
 	cout << "\nFitness value : " << best_indiv.fitness() << endl;
 }
 
+void Population::print_worst() {
+	Individual the_worst = individuals.at(track_worst);
+	cout << "Worst individual: "; the_worst.print_gene();
+	cout << "\nFitness value : " << the_worst.fitness() << endl;
+}
+
+void Population::print_best() {
+	Individual the_best = individuals.at(track_best);
+	cout << "Best individual: "; the_best.print_gene();
+	cout << "\nFitness value: " << the_best.fitness() << endl;
+}
+
 int main(void) {
 	Individual individual;
 	Population population;
+	Individual parent1 = population.tournament(2);
+	Individual parent2 = population.tournament(2);
+
+	cout << "Parent1 : "; parent1.print_gene();
+	cout << "\nParent2 : "; parent2.print_gene();
+	cout << endl;
 	
 	cout << rnorm(generator) << endl;
 	cout << boundary[0] << endl;
@@ -118,4 +183,17 @@ int main(void) {
 	cout << individual.fitness() << "\n\n";
 
 	population.print_population();
+	population.print_worst();
+	cout << "track best : " << track_best << endl;
+	cout << "track worst : " << track_worst << endl;
+
+	Individual achild;
+	population.update_population(achild);
+
+	cout << "\noffspring : "; achild.print_gene();
+	cout << "\n";
+	population.print_population();
+	population.print_worst();
+	cout << "track best : " << track_best << endl;
+	cout << "track worst : " << track_worst << endl;
 }
